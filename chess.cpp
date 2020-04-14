fde642ebe0755b73590ef96fc8b8915e4d7f562f
emcgill03 <emcgill03@hotmail.com>
2020-04-14 18:46:47 +0100

    Update with user interaction

--- chess.cpp
+++ chess.cpp
@@ -1,4 +1,5 @@
 #include "computer.h"
+#include "interface.h"
 
 //code I've been using to test computer class
 	//********************************************
@@ -25,8 +26,48 @@ int main()
 			cin >> n;
 			cout << endl << n << endl;
 			userturn = 0;
-			//clc (clear terminal)
-			//run user's code
+			system("cls");  
+			
+			while (board1.getStatus() != checkmate)  // While status of the game is not checkmate
+			{
+				cout << userturn << "'s turn." << endl;
+				cout << "Enter the coordinates of your starting position: " << endl;
+				cin >> startPos;
+				cout << "Enter the coordinates of where you want to move to: " << endl;
+				cin >> endPos;
+				
+				//Convert input to matrix coordinates that can be used by program
+				startColumn = letterToColumn(startPos.substr(0, 1));  // takes the column (char) of the input 
+				startRow = NumberToRow(startPos.substr(1, 2))          // takes the row (int) of the input
+				
+					
+				if (Check rules...)
+				{
+				board1.move(startPos, endPos);
+				}
+				
+				if (board1.getStatus() == check)
+				{
+					cout << Colours[board1.getTurn()] << "'s king is in check!" << endl;
+					
+				}
+
+				board1.printBoard();
+				cout << "________________________________________" << endl;
+			}
+
+			// Output when checkmate is true
+			if (userturn == 1)
+			{
+				cout << "Congratulations, YOU WIN!!!" << endl;
+				cout << "------------- END OF GAME -------------" << endl;
+			}
+			else
+			{
+				cout << "Computer wins!! Better luck next time" << endl;
+				cout << "------------- END OF GAME -------------" << endl;
+			}
+			
 			//run eoin's king situations
 			// - needs to check that if the king was still in check or if the user moved into a checkmate
 			// - needs to check if there's checkmate and display it
