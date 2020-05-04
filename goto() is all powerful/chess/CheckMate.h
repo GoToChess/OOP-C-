23982195087e0247330fd4e94c888c6775841571
DurkinEoin <62715791+DurkinEoin@users.noreply.github.com>
2020-05-04 18:15:04 +0100

    Update to CheckMate

--- goto() is all powerful/chess/CheckMate.h
+++ goto() is all powerful/chess/CheckMate.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "rules.h"
+#include "Rules.h"
 /**
 *Class Class Defining the king situatiosn throughout the game
 *
@@ -9,7 +9,7 @@
 *
 */
 
-class CheckMate	
+class CheckMate
 {
 private:
 	Rules K;
@@ -17,87 +17,53 @@ private:
 
 
 public:
-	int checkMate_B(char kingcolour, char attacking_colour, Piece Matrix[8][8]);
-	int checkMate_W(char kingcolour, char attacking_colour, Piece Matrix[8][8]);
+	int checkMate(char kingcolour, Piece Matrix[8][8]);
 
 };
 /**
 *@author Eoin Durkin
 *
-*Checks if the Black King is CheckMate
+*Checks if the King is CheckMate
 *
 *@param kingcolour the colour of the king thats turn it is to play
-*@param attacking_colour the colour of the piece which is attacking the king
 *@param Matrix[8][8] an 8x8 array of "Piece" objects which represent the current chess board to be analysed
 *@return returns a 1 if the Black King is CheckMate, Returns 0 if it is not
 */
-int CheckMate::checkMate_B(char kingcolour, char attacking_colour, Piece Matrix[8][8])
+
+int CheckMate::checkMate(char kingcolour, Piece Matrix[8][8])
 {
-	for (int a = 0; a < 8; a++) {
-		for (int b = 0; b < 8; b++) {
-			//Locates where the Black King is on the Board and Stores the location of it
-			if (Matrix[a][b].getColour() == 'B') {
-				if (Matrix[a][b].getType() == 'K') {
-					king_x = a;
-					king_y = b;
-				}
+	int king_x = 0, king_y = 0;
+	//find king
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (Matrix[i][j].getColour() == kingcolour && Matrix[i][j].getType() == 'K')
+			{
+				king_x = i;
+				king_y = j;
 			}
 		}
 	}
 
-	for (int c = 0; c < 8; c++) {
-		for (int d = 0; d < 8; d++) {
-			//Locates any other opponent pieces on the board, It checks if the Black King is in Check first, if so then checks
-			//if the King has any possible moves in which he can escape from Check if not return 1;
-			if ((K.incheck('B', Matrix) == 1) && (K.squareattack(king_x, king_y, 'W', Matrix) == 1)) 
-				{
-					return 1;//Black King is CheckMate
-				}
-			//If King can escape return 0
-			else
-				{
-					return 0;//Black King is not in checkmate continue
-				}
-			}
-		}
-}
-/**
-*@author Eoin Durkin
-*
-*Checks if the White King is CheckMate
-*
-*@param kingcolour the colour of the king thats turn it is to play
-*@param attacking_colour the colour of the piece which is attacking the king
-*@param Matrix[8][8] an 8x8 array of "Piece" objects which represent the current chess board to be analysed
-*@return returns a 1 if the White King is CheckMate, Returns 0 if it is not
-*/
-int CheckMate::checkMate_W(char kingcolour, char attacking_colour, Piece Matrix[8][8])
-{
-	for (int a = 0; a < 8; a++) {
-		for (int b = 0; b < 8; b++) {
-			//Locates where the White King is on the Board and Stores the location of it
-			if (Matrix[a][b].getColour() == 'W') {
-				if (Matrix[a][b].getType() == 'K') {
-					king_x = a;
-					king_y = b;
-				}
-			}
-		}
-	}
-	
-	for (int c = 0; c < 8; c++) {
-		for (int d = 0; d < 8; d++) {
-			//Locates any other opponent pieces on the board, It checks if the White King is in Check first, if so then checks
-			//if the King has any possible moves in which he can escape from Check if not return 1;
-			if ((K.incheck('W',Matrix) == 1) && (K.squareattack(king_x, king_y, 'B', Matrix)== 1))
-			{
-				return 1;//White King is CheckMate
-			}
-			//If King can escape return 0
-			else
-			{
-				return 0;//White King is not CheckMate
-			}
-		}
-	}
+	//check if he is allowed to move to any square around him
+	int a = K.moveLegal(king_x, king_y, king_x + 1, king_y, Matrix);
+	int b = K.moveLegal(king_x, king_y, king_x + 1, king_y + 1, Matrix);
+	int c = K.moveLegal(king_x, king_y, king_x + 1, king_y - 1, Matrix);
+
+	int d = K.moveLegal(king_x, king_y, king_x, king_y - 1, Matrix);
+	int e = K.moveLegal(king_x, king_y, king_x, king_y + 1, Matrix);
+
+	int f = K.moveLegal(king_x, king_y, king_x - 1, king_y - 1, Matrix);
+	int g = K.moveLegal(king_x, king_y, king_x - 1, king_y, Matrix);
+	int h = K.moveLegal(king_x, king_y, king_x - 1, king_y + 1, Matrix);
+
+
+	//if he can't move to any square around him & he is also in check, that is checkmate
+		if ((a == 0 && b == 0 && c == 0 && d == 0 && e == 0 && f == 0 && g == 0 && h == 0) && (K.incheck(kingcolour, Matrix) == 1)) 
+			return 1;
+		else 
+			return 0;
+
+
 }
